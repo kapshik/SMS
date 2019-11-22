c06fdbaca800a2f5b41174528efdcc4d8af254d0
kapshik <7329220+kapshik@users.noreply.github.com>
2019-11-22 18:03:48 +0900

    Change Generic

--- Source/src/main/java/com/ksk/sms/service/view/SmsViewService.java
+++ Source/src/main/java/com/ksk/sms/service/view/SmsViewService.java
@@ -2,21 +2,21 @@ package com.ksk.sms.service.view;
 
 import com.ksk.sms.model.SmsViewModel;
 
-public interface SmsViewService {
+public interface SmsViewService <T extends SmsViewModel>{
 
-    public <T extends SmsViewModel> T init();
+    public T init();
 
-    public <T extends SmsViewModel> T search(T inModel);
+    public T search(T inModel);
 
-    public <T extends SmsViewModel> T create(T inModel);
+    public T create(T inModel);
 
-    public <T extends SmsViewModel> T update(T inModel);
+    public T update(T inModel);
 
-    public <T extends SmsViewModel> T delete(T inModel);
+    public T delete(T inModel);
 
-	public <T extends SmsViewModel> T customerChange(T inModel);
+　  public T customerChange(T inModel);
 
-    public <T extends SmsViewModel> T branchChange(T inModel);
+    public T branchChange(T inModel);
 
-    public <T extends SmsViewModel> T close(T inModel);
+    public T close(T inModel);
 }
