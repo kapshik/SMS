d5d25dc3f587a432b419eff4ae7445c2d2f3df83
kapshik <kapshik@hotmail.com>
2019-12-01 23:39:04 +0900

    Test追加

---
+++ Source/src/test/java/com/ksk/sms/service/view/impl/OrderServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.ksk.sms.service.view.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrderServiceImplTest {
+
+	@Test
+	void test() {
+		fail("まだ実装されていません");
+	}
+
+}
