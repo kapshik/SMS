7b60c0781c6c743bc07acbdee0c2dde31971bbe5
kapshik <kapshik@hotmail.com>
2019-12-01 23:02:16 +0900

    Junit実験

---
+++ Source/src/test/java/com/ksk/sms/service/view/impl/SmsCommonServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.ksk.sms.service.view.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SmsCommonServiceImplTest {
+
+	@Test
+	public void test() {
+		fail("まだ実装されていません");
+	}
+
+}
