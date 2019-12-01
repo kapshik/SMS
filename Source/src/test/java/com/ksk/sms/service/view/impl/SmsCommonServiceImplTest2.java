412f033d129de83eb2498f5cfcb362583a679eab
kapshik <kapshik@hotmail.com>
2019-12-01 23:54:08 +0900

    Test

---
+++ Source/src/test/java/com/ksk/sms/service/view/impl/SmsCommonServiceImplTest2.java
@@ -0,0 +1,49 @@
+package com.ksk.sms.service.view.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SmsCommonServiceImplTest2 {
+
+	@Test
+	void testInit() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testSearch() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testCreate() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testDelete() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testCustomerChange() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testBranchChange() {
+		fail("まだ実装されていません");
+	}
+
+	@Test
+	void testClose() {
+		fail("まだ実装されていません");
+	}
+
+}
